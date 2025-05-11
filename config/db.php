82e09257be709b170e63a840c0399e1ddd8a9535
Notnochka <pali4an@gmail.com>
2025-05-12 00:43:09 +0700

    исправление

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=taskforce',
+    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=task_force_anton',
     'username' => 'root',
     'password' => 'root',
-    'charset' => 'utf8',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
